2b212685a1e7cbe0c2500a6ec2f9e6b48a2475b7
natsyneos <nat.molina@syneoshealth.com>
2026-02-19 16:39:07 -0800

    import path fix

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -4,7 +4,7 @@ import { LandingScreen } from './components/LandingScreen';
 import { QuizScreen } from './components/QuizScreen';
 import { FinalScreen } from './components/FinalScreen';
 import { PasswordGate } from './components/PasswordGate';
-import finalBgImage from '../assets/5057818b1ad83dbb2d772c8571a841a2613175ce.png';
+import finalBgImage from './assets/5057818b1ad83dbb2d772c8571a841a2613175ce.png';
 
 type Screen = 'landing' | 'quiz' | 'final';
 
